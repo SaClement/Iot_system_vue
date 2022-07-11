b6c88cdcfe92b51c5384197e3bbcc43a6f5fa9b7
SaClement <15213075743@163.com>
2022-07-11 09:51:40 +0800

    dark loading

---
+++ src/views/dashboard/analysis/components/TopChart/index.vue
@@ -0,0 +1,35 @@
+<script setup lang='ts'>
+import { toggleDark } from '@/composables';
+
+const toast = () => {
+  ElMessage.warning('Error')
+}
+</script>
+
+<template>
+  <div>dwad1</div>
+  <el-button @click="toast">El Message</el-button>
+  <el-button @click="toggleDark()" type="primary">点击</el-button>
+  <el-button @click="toggleDark()" type="success">点击</el-button>
+  <el-button @click="toggleDark()" type="warning">点击</el-button>
+  <el-button @click="toggleDark()" type="danger">点击</el-button>
+  <el-button @click="toggleDark()" type="info">点击</el-button>
+  <p>
+    Full Example:
+    <a
+      href="https://github.com/element-plus/element-plus-vite-starter"
+      target="_blank"
+    >element-plus-vite-starter</a>
+    | On demand Example:
+    <a
+      href="https://github.com/element-plus/unplugin-element-plus"
+      target="_blank"
+    >unplugin-element-plus/examples/vite</a>
+  </p>
+</template>
+
+<style lang='scss'>
+.ep-button{
+  margin: 10px
+}
+</style>
---
+++ src/views/dashboard/analysis/index.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+import { TopChart, DataCard, BottomPart } from './components';
+</script>
+
+<template>
+  <el-space :vertical="true" >
+    <top-chart />
+    <data-card />
+    <bottom-part />
+  </el-space>
+</template>
+
+<style lang='' scoped></style>
